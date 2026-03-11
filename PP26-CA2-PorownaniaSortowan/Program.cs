f5d1e60be6e32c91590794dc470db00503c8a7f5
Filip Celejewski <FilipCelejewski@wp.pl>
2026-03-11 13:49:05 +0100

    Dodaj pliki projektów.

---
+++ PP26-CA2-PorownaniaSortowan/Program.cs
@@ -0,0 +1,13 @@
+﻿using PP26_CA2_PorownaniaSortowan;
+
+public class Program
+{
+    public static void Main()
+    {
+        Sorter sorter = new Sorter();
+        int[] inputTable = { 5, 2, 9, 1, 5, 6 };
+
+        int[] bubbleSortedTable = sorter.BubbleSort(inputTable);
+        Console.WriteLine("Bubble Sorted Table: " + string.Join(", ", bubbleSortedTable));
+    }
+}
---
+++ PP26-CA2-PorownaniaSortowan/Sorter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PP26_CA2_PorownaniaSortowan
+{
+    public class Sorter
+    {
+        public int[] BubbleSort(int[] inputTable)
+        {
+            for(int i = 0; i < inputTable.Length; i++)
+            {
+                for (int j = 0; j < inputTable.Length - 1; j++)
+                {
+                    if (inputTable[j] > inputTable[j + 1])
+                    {
+                        int temp = inputTable[j];
+                        inputTable[j] = inputTable[j + 1];
+                        inputTable[j + 1] = temp;
+                    }
+                }
+            }  
+
+            return inputTable;
+        }
+    }
+}
